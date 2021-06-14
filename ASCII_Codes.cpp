c7e707e74c1bb5861c499afac86a0ea1066dceeb
Ahmed Othman <75481578+ahmed-othman11@users.noreply.github.com>
2021-06-14 16:58:30 +0200

    Create ASCII_Codes.cpp
    Program to Convert One Character to ASCII Code.

---
+++ ASCII_Codes.cpp
@@ -0,0 +1,12 @@
+
+//	- Program to Convert One Character to ASCII Code.
+
+#include <iostream>
+using namespace std;
+int main() {
+	char cha;
+	cout << "Enter the character to convert to ASCII :  ";
+	cin >> cha;
+	cout << "The result of convert is: " << int(cha) << endl;
+	return 0;
+}
